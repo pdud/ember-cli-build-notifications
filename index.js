3ba059072bc035f1652eaea3bf2d1202c01fc2bf
Philip Dudley <pdudley89@gmail.com>
2015-07-11 20:10:47 -0400

    Add optional config file

--- index.js
+++ index.js
@@ -2,11 +2,16 @@
 'use strict';
 
 var notifier = require('./lib/notifier');
+var Config = require('./lib/config');
 
 module.exports = {
   name: 'ember-cli-build-notifications',
 
   buildError: function(error) {
-    notifier.notify(error);
+    var config = Config.load(this.project.root);
+
+    if (config.buildError.notify) {
+      notifier.notify(error);
+    }
   }
 };
---
+++ tests/unit/config-nodetest.js
@@ -0,0 +1,19 @@
+/* jshint node: true */
+'use strict';
+
+var Config = require('../../lib/config');
+var path = require('path');
+var expect = require('chai').expect;
+
+describe('load', function() {
+
+  it('initializes with defaults', function() {
+    var config = Config.load();
+    expect(config.buildError.notify).to.eq(true);
+  });
+
+  it('allows settings in config to take precedence', function() {
+    var config = Config.load(path.join(__dirname, '..', 'fixtures'));
+    expect(config.buildError.notify).to.eq(false);
+  });
+});
